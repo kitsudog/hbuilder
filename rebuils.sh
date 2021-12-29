9e3f87ef52fa28da5ebdae8b4a097a7ac4ddc833
dave-luo <kitsudo163@163.com>
2021-12-29 21:12:40 +0800

    提交重新构建的脚本

--- rebuils.sh
+++ rebuils.sh
@@ -1,11 +1,8 @@
-rsync -aP /Applications/HBuilderX.app/Contents/HBuilderX/plugins .
+set -x
+rsync -a /Applications/HBuilderX.app/Contents/HBuilderX/plugins .
 
 NODE_VERSION=$(./plugins/node/node --version|cut -c2-)
 ESBUILD_VERSION=$(./plugins/uniapp-cli-vite/node_modules/esbuild-darwin-64/bin/esbuild --version)
 VERSION=$(cat ./plugins/uniapp-cli-vite/package.json|grep version|cut -d: -f2|cut -d- -f2|cut -d'"' -f1)
-if [ $NODE_VERSION = 12.22.1 ]
-then
-  docker build . -f Dockerfile -t kitsudo/hbuilder:${VERSION} --build-arg NODE_VERSION=${NODE_VERSION} --build-arg ESBUILD_VERSION=${ESBUILD_VERSION}
-else
-  echo 请更新Dockerfile
-fi
+git clean -xdf
+docker build --platform linux/amd64 . -f Dockerfile -t kitsudo/hbuilder:${VERSION} --build-arg NODE_VERSION=${NODE_VERSION} --build-arg ESBUILD_VERSION=${ESBUILD_VERSION}
