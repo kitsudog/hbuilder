b2cd72d83dd1e16b5f50063fa4927027b830ac6d
dave-luo <kitsudo163@163.com>
2022-11-25 06:29:41 +0800

    /app=>/demo

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV NODE_ENV=production \
     UNI_INPUT_DIR=/app \
     UNI_OUTPUT_DIR=/app/unpackage/dist/build/h5 \
     UNI_PLATFORM=h5
-COPY app /app
+COPY app /demo
 COPY plugins/ /node
 ARG ESBUILD_VERSION=0.13.15
 RUN mkdir /es && cd /es && npm i esbuild@${ESBUILD_VERSION} && cp -r /es/node_modules/esbuild /node/uniapp-cli-vite/node_modules/ && cp -r /es/node_modules/esbuild-linux-64 /node/uniapp-cli-vite/node_modules/ && rm -fr /root/.npm /es
--- rebuils.sh
+++ rebuils.sh
@@ -4,5 +4,6 @@ rsync -a /Applications/HBuilderX.app/Contents/HBuilderX/plugins .
 NODE_VERSION=$(./plugins/node/node --version|cut -c2-)
 ESBUILD_VERSION=$(./plugins/uniapp-cli-vite/node_modules/esbuild-darwin-64/bin/esbuild --version)
 VERSION=$(cat ./plugins/uniapp-cli-vite/package.json|grep version|cut -d: -f2|cut -d- -f2|cut -d'"' -f1)
+docker build --platform linux/amd64 . -f Dockerfile-full -t kitsudo/hbuilder:${VERSION}-full
 git clean -xdf
 docker build --platform linux/amd64 . -f Dockerfile -t kitsudo/hbuilder:${VERSION} --build-arg NODE_VERSION=${NODE_VERSION} --build-arg ESBUILD_VERSION=${ESBUILD_VERSION}
