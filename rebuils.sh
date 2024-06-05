f0be6e92fca649732240f5746aa6c18239e42f34
dave-luo <kitsudo163@163.com>
2024-06-06 04:24:43 +0800

    更新版本

--- rebuils.sh
+++ rebuils.sh
@@ -2,8 +2,7 @@ set -x
 rsync -a /Applications/HBuilderX.app/Contents/HBuilderX/plugins .
 
 NODE_VERSION=$(./plugins/node/node --version|cut -c2-)
-ESBUILD_VERSION=$(./plugins/uniapp-cli-vite/node_modules/esbuild-darwin-64/bin/esbuild --version)
-VERSION=$(cat ./plugins/uniapp-cli-vite/package.json|grep version|cut -d: -f2|cut -d- -f2|cut -d'"' -f1)
-docker build --platform linux/amd64 . -f Dockerfile-full -t kitsudo/hbuilder:${VERSION}-full
+ESBUILD_VERSION=$(./plugins/uniapp-cli-vite/node_modules/esbuild/bin/esbuild --version)
+VERSION=$(cat ./plugins/uniapp-cli-vite/package.json|jq .version -r)
 git clean -xdf
-docker build --platform linux/amd64 . -f Dockerfile -t kitsudo/hbuilder:${VERSION} --build-arg NODE_VERSION=${NODE_VERSION} --build-arg ESBUILD_VERSION=${ESBUILD_VERSION}
+docker build --platform linux/amd64 . -f .ci/Dockerfile -t kitsudo/hbuilder:${VERSION} --build-arg NODE_VERSION=${NODE_VERSION} --build-arg ESBUILD_VERSION=${ESBUILD_VERSION} --progress plain
